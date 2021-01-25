06a79c5572a08e167ac2d6588a9a1eef2c258da5
LaetitiaDebruyne <67645912+LaetitiaDebruyne@users.noreply.github.com>
2021-01-25 13:21:37 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,473 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" href="static/fontawesome/css/all.css" />
+  <link rel="stylesheet" href="style.css" />
+
+  <!-- Keywords -->
+  <meta data-n-head="ssr" name="keywords" content="La Maison Cool’heure est un centre dédié au développement personnel des
+          enfants, un espace-temps bienveillant pour y développer une ontologie
+          optimiste et volontariste.">
+
+  <!--liens pour favicon-->
+  <link rel="apple-touch-icon" sizes="180x180" href="static/images/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="static/images/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="static/images/favicon//favicon-16x16.png">
+
+  <!-- <link rel="manifest" href="/site.webmanifest"> -->
+  <link rel="mask-icon" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+
+  <title>Maison Cool'Heure</title>
+</head>
+
+<body>
+
+  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-orange">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <img class="mx-2" src="static/images/logo.png" alt="logo" width="30" height="30" />
+      <a class="navbar-brand text-warning" href="#">COOL'HEURE</a>
+    
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#1">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#2">Equipe</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#3">Rendez-vous</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#4">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#5">Galerie</a>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row p-4 p-md-5 mb-4 text-white rounded bg-orange2 mt-4">
+      <h1 class="display-4 font-italic">Maison Cool'Heure</h1>
+      <div class="col-md-6 px-0">
+        <p class="lead my-3 ">La Maison Cool’heure est un centre dédié au développement personnel des
+          enfants, un espace-temps bienveillant pour y développer une ontologie
+          optimiste et volontariste.</p>
+        <div class="bienvenue"><i class="fas fa-praying-hands"></i>
+          Bienvenue
+          <i class="fas fa-praying-hands"></i>
+
+        </div>
+        <div class="resume">
+          <p>
+            Heures d'ouverture :
+            du lundi au vendredi 09:00 - 19:00
+          <p><i class="fas fa-phone-alt"> 0474 12 37 28</i></p>
+          <!-- <p><i class="fas fa-envelope"> info@maisoncoolheure.be</i></p> -->
+        </div>
+      </div>
+      <div class="col-md-6 px-0">
+        <img class="img-fluid" src="static/images/maison/12.jpg" alt="">
+      </div>
+    </div>
+
+    <div>
+      <h2 class="text-center">Retrouvez-nous sur notre page <a href="https://www.facebook.com/Maisoncoolheure/" class="link-danger">Facebook</a></H2>
+    </div>
+
+    <hr>
+
+
+    <div class="effect-3">
+      <div class="effect-img">
+        <img class="img-fluid" src="static/images/header/header-pic.jpg" alt="Image du Header">
+      </div>
+    </div><br />
+
+
+    <div>
+      <h2>Repartons à zéro !</h2>
+      <p>Célébrez la saison des Changements.
+    </div>
+
+    <hr>
+
+
+ 
+    <section id="1">
+      <div class="my-5"></div>
+      
+
+      <h2 class="my-5">Accueil</h2>
+
+      <p class="ms-2 text-center my-4">
+
+        <strong>Maison d'accueil et centre de développement personnel pour petits & grands</strong><br /><br />
+
+        Une aventure vers Soi, les Autres & la Nature <br />
+
+      <blockquote class="blockquote text-center">
+
+        Pourquoi un centre de développement personnel ? <br />
+
+        Parce que je crois que le monde serait meilleur <br />
+
+      </blockquote>
+
+      <ul>
+        <li class="list-group-item disabled">si la recherche en développement personnel était accessible à tous</li>
+        <li class="list-group-item disabled">si nous savions cmt restaurer le lien avec Nous-même </li>
+        <li class="list-group-item disabled">si nous savions cmt entretenir des liens sains avec les Autres</li>
+        <li class="list-group-item disabled">si nous savions cmt respecter notre lien à la Nature</li>
+      </ul><br />
+
+      <p>
+        Dans cette optique, nous proposons ici :
+        <p />
+
+      <ul>
+        <li class="list-group-item disabled">des pratiques de bien-être,</li>
+        <li class="list-group-item disabled">des soins thérapeutiques </li>
+        <li class="list-group-item disabled"> et des ateliers d’échange </li>
+      </ul><br />
+
+      <p class="text-center">Vous trouverez ici un espace hors du temps et une équipe de soutien face aux difficultés de la vie et
+        au mal de vivre.<br />
+        Apprendre à regarder le passé avec gratitude, le
+        présent avec conscience et le futur avec un esprit créatif
+        <p />
+
+      <h3>La Maison Cool’heure un espace de ressourcement :</h3><br />
+
+      <p class="text-center">Venez vibrer et vous défouler lors des Ronde de tambours.Venez rire et vous extérioriser lors des
+        séances de Yoga du rire.Venez vibrer et voyagez au pays des sonsVenez
+        sourire et vous ressourcer lors des Méditations.Venez lire et boire un
+        thé quand vous le désirez. (Sur RDV uniquement)
+        <p />
+
+      <h3>La Maison Cool’heure un espace de soin :</h3><br />
+
+      <ul>
+        <li class="list-group-item disabled">Cicatriser des 5 blessures de l’enfance : rejet,
+          abandon, trahison, humiliation, injustice</li>
+        <li class="list-group-item disabled">Soulager les angoisses, la dépression, l’addiction, la phobie sociale</li>
+        <li class="list-group-item disabled"> Apaiser ses émotions violentes et traumas</li>
+      </ul><br />
+
+      <p class="text-center">Prendre soin de son âme, c’est aussi retourner vers
+        la neutralité, plonger dans un calme énergique, un peps serein. La
+        logosynthèse et la méthode Peace vous y amène tout en douceur.Prendre
+        soin de son corps, c’est aussi accepter de démêler des points de tension
+        et de se remplir d’énergie. Les soins énergétiques, le magnétisme et la
+        sonothérapie vous font vous sentir mieux dans votre peau.
+        <p />
+
+      <h3>La Maison Cool’heure un espace d’échange :</h3><br />
+
+      <p><u>Formations & conférences :
+        </u>
+        <p />
+      <p>La formation continue est une donnée essentielle dans le développement personnel de
+        chacun d’entre nous. C’est pourquoi différents formateurs viendront
+        partager des outils concrets à mettre en pratique dans notre vie de tous
+        les jours. Surveillez les événements proposés par la Maison Cool’heure.
+        <p />
+
+      <p><u>Blabla Cool :
+        </u></p>
+
+      <p>Un espace de discussions pour explorer les trésors de
+        l’intelligence collective. Prendre soin de son mental, c’est aussi le
+        nourrir de réflexion, de débats, de remise en question. C’est bousculer
+        sa zone de confort pour découvrir de nouveaux horizons et des réalités
+        alternatives différentes de la sienne dans un cadre bienveillant.Dans
+        l’idée d’une interaction bienveillante, saine et fructueuse, les groupes
+        sont limités à 10 personnes.
+      </p>
+    </section>
+
+
+    <section id="2">
+    <div class="my-5"></div>
+    
+      <h2 class="my-5">Equipe</h2>
+      <div class="row">
+        <div class="member img-thumbnail  col-lg-4 ">
+          <img src="static/images/photos/photo-sarah.jpeg" class="img-fluid" alt="Photo de Sarah">
+
+
+
+          <h2 class="mb-0 pm-0">Sarah Lemaire</h2>
+          <!-- <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p> -->
+          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p> -->
+        </div>
+        <div class="member img-thumbnail  col-lg-4">
+   
+          <img src="static/images/photos/photo-yahel.JPG" class="img-fluid" alt="Photo de Sarah">
+
+   
+          
+          <h2 class="mb-0 pm-0">Yahel Perez</h2>
+          <!-- <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p> -->
+          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p> -->
+        </div>
+        <div class="member img-thumbnail  col-lg-4">
+          <img src="static/images/photos/photo-lydia.jpg" class="img-fluid" alt="Photo de Sarah">
+
+          <h2 class="mb-0 pm-0">Lydia Amellal</h2>
+          <!-- <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p> -->
+          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p> -->
+        </div>
+
+
+        <p class="mt-4"><img class="img-fluid" src="static/images/photos/showing42.jpg" /></p>
+      </div>
+    </section>
+
+    <hr>
+
+ 
+    <section id="3">
+    <div class="my-5"></div> 
+
+      <h2 class="my-5">Rendez-vous</h2>
+      <p class="text-center">Vous pouvez appeler pour prendre un rendez-vous</p>
+      <p class="text-center"><i class="fas fa-phone-alt"> 0474 12 37 28</i></p>
+      <div class="adress text-center">
+        Rue Robert Scott, 40 1180 Uccle
+      </div>
+      <iframe class="image-adresse2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2521.7280676741175!2d4.338933625635121!3d50.79914799662577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c5ae2dd23cd3%3A0x4c5807902b3fd69c!2sRue%20Robert%20Scott%2040%2C%201180%20Uccle!5e0!3m2!1sfr!2sbe!4v1607003112076!5m2!1sfr!2sbe" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+      <!-- <a href="https://www.google.com/maps/d/edit?mid=169A_fRYPPf-sL-r6U03zTjK5Q0ut1ETN&usp=sharing"></a> -->
+    </section>
+
+    <hr>
+
+
+    
+    <section id="4">
+    <div class="my-5"></div>
+      <h2 class="my-5">Services</h2>
+
+      <p class="text-center">
+
+        <strong>Accueils, thérapies, ateliers, conférences pour un mieux-être.<br />
+
+          Notre chemin est unique, personnel et empli de trésors.
+
+          Pour abandonner les idées négatives et les reformuler en pensées positives.</strong>
+      </p>
+      <div class="list-group ">
+        <a href="#" title="servives" class="list-group-item disabled text-center">NOS SERVICES</a>
+
+
+        <br />
+        <a "list-group-item list-group-item-danger"> Groupe PEACE</a>
+        <a "list-group-item list-group-item-danger"> Méthode PEACE</a>
+        <a "list-group-item list-group-item-danger"> Logosynthèse</a>
+
+
+
+
+        <a "list-group-item list-group-item-danger"> Voyage sonore</a>
+        <a "list-group-item list-group-item-danger"> Ronde des tambours</a>
+        <a "list-group-item list-group-item-danger"> Yoga du rire</a>
+        <a "list-group-item list-group-item-danger"> Méditation</a>
+
+      </div>
+  </div>
+
+  <br />
+
+  <div>
+    <div>
+      <h4>Ostéthérapie : Méthode Poyet - Lydia Ame</h4>
+      <p class="price">1 heure • 60 €</p>
+
+      <p>Contre les souffrances corporelles d’origine mécanique et/ou émotionnelles, ponctuelles ou chroniques, aux
+        niveaux articulaire, musculaire, ligamentaire, viscéral…
+
+        Déroulement d'une séance :
+      <ul>
+        <li class="list-group-item list-group-item-danger"> Anamnèse (étude des signes) : déceler les problèmes affectant le corps tout entier par le crâne.</li>
+        <li class="list-group-item list-group-item-danger">Corrections au Sacrum, lieu de correction général</li>
+        <li class="list-group-item list-group-item-danger">Libérations locales des restrictions de mouvement : du bassin en remontant la colonne vers le crâne, avec
+          des détours viscéral, pied, diaphragme.</li>
+      </ul>
+      Les gestes de correction sont légers " comme le battement d’ailes d'un papillon</p>
+    </div>
+
+    <div>
+      <h4>Thérapie individuelle - Yahel Perez</h4>
+      <p class="price">1 heure • Prix variable</p>
+      <p>Yahel Perez, psychologue, vous propose des méthodes thérapeutiques issues de recherches récentes dans le domaine
+        neuro-cognitif et quantique.
+
+        A la recherche de résultats durables pour vous libérer de vos blessures, traumas, difficultés relationnelles,
+        peurs, ... ?
+
+        Ces méthodes amènent un réel soulagement, nettoient vos traumas et autres blessures .... souvent avec une facilité
+        déconcertante !</p>
+      <p>
+        Pour en savoir plus, retrouvez-la sur :</p>
+      <a href="https://www.retrouverlecheminverssoi.com"></a>
+
+    </div>
+
+    <div>
+      <h4>Ronde des Tambours (COVID : suspendu)</h4>
+      <p class="price">1 heure • Prix variable</p>
+      <p>Retrouvez-nous une fois par mois pour libérer les tensions et améliorer votre bien-être au travers des
+        percussions.
+
+        Venez donc vibrer avec nous !
+        Que vous ayez des compétences musicales ou non,
+        Que vous ayez un instrument ou pas.</p>
+    </div>
+
+    <div>
+      <h4>Groupe PEACE - Yahel Perez (suspendu)</h4>
+      <p class="price">2 heures • 80 $</p>
+      <p>Faire un travail thérapeutique en groupe, c’est partager notre vécu avec d’autres et entendre le leur. C’est
+        recevoir un autre regard que celui que l’on porte sur notre situation.
+
+      <ul>
+        <li class="list-group-item list-group-item-danger">En début et fin de séance, un exercice de cohérence cardiaque de 5 à 10 minutes</li>
+        <li class="list-group-item list-group-item-danger">un moment de partage. Pour s’exprimer, réfléchir sur soi en fonction de ce qui vient des autres, choisir la
+          situation qu’on voudra travailler en groupe</li>
+        <li class="list-group-item list-group-item-danger">un exercice en méthode PEACE </li>
+      </ul>
+      Les groupes PEACE ne sont pas destinés à remplacer les séances individuelles. C’est une autre approche, une
+      possibilité en plus, simplement.</p>
+
+    </div>
+    </section>
+
+    <hr>
+
+
+    <section id="5" class="mt-5">
+    <div class="my-5"></div>
+      <h2 class="my-5">Galerie</h2>
+
+
+      <p> Toute la Galerie <a class="link-danger" href="galery.html">ICI</a></p><br />
+
+      <div class="container">
+
+  <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="image-block">
+             <img src="static/images/maison/showing1.png">
+            </div>
+           
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="image-block">
+           <img src="static/images/maison/showing2.png" />
+            </div>
+           
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="image-block">
+              <img src="static/images/maison/showing3.png"/>
+            </div>
+        
+           
+        </div>
+    </div>
+
+    </section>
+
+
+   <br/><br/>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <ol class="carousel-indicators">
+        <?php
+        for ($i = 0; $i < 12; $i++) {
+          if ($i <= 0) {
+        ?>
+            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?= $i ?>" class="active"></li>
+          <?php } else { ?>
+            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?= $i ?>"></li>
+        <?php
+          }
+        }
+        ?>
+      </ol>
+
+      <div class="carousel-inner">
+        <?php
+        for ($i = 1; $i < 12; $i++) {
+          if ($i <= 1) {
+        ?>
+            <div class="carousel-item active">
+              <img src="static/images/maison/<?= $i ?>.jpg" class="d-block w-100" alt="...">
+            </div>
+          <?php } else { ?>
+            <div class="carousel-item">
+              <img src="static/images/maison/<?= $i ?>.jpg" class="d-block w-100" alt="...">
+            </div>
+        <?php
+          }
+        }
+        ?>
+      </div>
+
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </a>
+    </div>
+
+    </section>
+
+    <hr>
+
+    <footer class="text-center">
+      <p> &copy;Maison Cool'Heure</p>
+      <a class="link-danger" href="mentions.html">Mentions Légales</a>
+      <!-- <p><a href="mailto:info@maisoncoolheure.be ">info@maisoncoolheure.be </a></p> -->
+    </footer>
+
+
+  </div>
+
+
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,17 @@
+import './styles.scss';
+import footer from './static/images/footer/footer.jpg';
+
+import $ from 'jquery';
+import 'bootstrap';
+// $('body').append('jquery + bootstrap works!');
+
+// document.body.innerHTML += `<img src="${footer}" width="400px"/>`;
+
+
+// $('.carousel').carousel({
+//   interval: 2000
+// });
+
+
+// var myCarousel = document.querySelector('.carousel');
+// var carousel = new bootstrap.Carousel(myCarousel);
